1fd7bc575f0fc21af26ee0974e5418866cd7f70b
Michael Golden <MichaelAGolden@users.noreply.github.com>
2023-10-21 14:11:05 -0500

    feat: refactored card.py and deck.py to cardecky.py; implemented @dataclass decorator to make more simple

---
+++ src/poker/cardecky.py
@@ -0,0 +1,60 @@
+import random
+from dataclasses import dataclass, field
+
+# Card and Deck classes combined into one file
+# Uses dataclass and field for simplicity
+
+
+@dataclass
+class Card:
+    suit: str
+    rank: str
+
+    def __post_init__(self):
+        self.card = self.rank + self.suit
+
+    def __repr__(self) -> str:
+        return self.card
+
+
+@dataclass
+class Deck:
+    deck: list = field(default_factory=list, init=False)
+    cards_used: int = 0
+    suits = ['C', 'H', 'D', 'S']
+    ranks = ['2', '3', '4', '5', '6',
+             '7', '8', '9', '0', 'J', 'Q', 'K', 'A']
+
+    def __post_init__(self):
+        for suit in self.suits:
+            for rank in self.ranks:
+                self.deck.append(Card(suit, rank))
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+        self.cards_used = 0
+
+    def cardsLeft(self):
+        return len(self.deck) - self.cards_used
+
+    def deal_card(self):
+        if self.cards_used >= len(self.deck):
+            print("Error: There are no cards left in the deck.")
+        else:
+            self.cards_used += 1
+            return self.deck[self.cards_used - 1]
+
+    def deal_cards(self, num):
+        if (self.cards_used + num) > len(self.deck):
+            print("Error: There are not enough cards left in the deck.")
+        else:
+            deal = []
+            for i in range(num):
+                deal.append(self.deal_card())
+            return deal
+
+    def __str__(self):
+        tmp_deck = "Deck: "
+        for c in self.deck:
+            tmp_deck += str(c) + " "
+        return tmp_deck
