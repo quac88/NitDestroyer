0448c138e67bebeb1f2d116b63160f1423dadc25
“quac88” <mac2@thrasher.com>
2023-10-21 23:49:40 -0500

    feat: update the run.py file to include betting logic from the Game class

--- src/poker/run.py
+++ src/poker/run.py
@@ -1,49 +1,77 @@
 from cardecky import Deck
-from game import Pot, Dealer, Player, Table
+from game import Pot, Dealer, Player, Table, Game
 
+# constants for betting - these are the max bets for each betting round
+ANTE = 1
+PRE_FLOP_LIMIT = 2
+FLOP_LIMIT = 4
+TURN_LIMIT = 4
+
+START_STACK = 200
 
 def main():
     # create a deck
     deck = Deck()
-    # shuffle the deck
-    deck.shuffle()
+
     # create a pot
     pot = Pot()
+
     # create a dealer
-    dealer = Dealer(deck=deck, pot=pot)
+    dealer = Dealer(pot=pot, deck=deck)
+
     # create a table
-    table = Table(seats=8)
-    # create players
-    player1 = Player(name="player1", stack=200, hand=[],
-                     status=True, chips_in_play=0)
-    player2 = Player(name="player2", stack=200, hand=[],
-                     status=True, chips_in_play=0)
-    players: list[Player] = [player1, player2]
-    # seat players
-    table.seat_player(player=player1, seat=0)
-    table.seat_player(player=player2, seat=1)
-    # move button
-    dealer.move_button(players=table.seats)
-    # print who the button is
-    print("Button: " + str(table.seats[dealer.button]))
-    # post antes
-    for player in players:
-        player.post_ante(ante=1)
-    # update pot
-    pot.add_to_pot(amount=2)
-    # print pot
-    print("Pot: " + str(pot.total))
-    # print each players stack
-    print("Player1 stack: " + str(player1.stack))
-    print("Player2 stack: " + str(player2.stack))
-    # deal 1 card to each player beggining to the seat left of the button
-    dealer.deal_hand(players=table.seats)
-    # print each players hand
-    player1_hand = player1.hand[0]
-    player2_hand = player2.hand[0]
-    print("Player1 hand: " + str(player1_hand))
-    print("Player2 hand: " + str(player2_hand))
+    table = Table(seats=9)
 
+    # create two players
+    player0 = Player(player_ID=0, stack=START_STACK, hand=[], status=True, chips_in_play=0)
+    player1 = Player(player_ID=1, stack=START_STACK, hand=[], status=True, chips_in_play=0)
+    # create a list of players
+    players: list[Player] = [player0, player1]
 
+    # seat the players
+    table.seat_player(player=player0, seat=0)
+    table.seat_player(player=player1, seat=1)
+
+    # shuffle the deck
+    deck.shuffle()
+
+    # set the button
+    dealer.move_button(players=players)
+
+    # set the button to default
+    button = 0
+
+    # create a game
+    game = Game(players=players, dealer=dealer, betting_limit=PRE_FLOP_LIMIT)
+
+    # post the antes
+    player0.post_ante(ante=ANTE, pot=pot) # player 0 puts in 1 chip to be eligable to play
+    player1.post_ante(ante=ANTE, pot=pot) # player 1 puts in 1 chip to be eligable to play
+
+    # deal the hand
+    dealer.deal_hand(players=players)
+    print(f"Player0 has hand: {player0.hand}")
+    print(f"Player1 has hand: {player1.hand}")
+
+    print("PRE-FLOP")
+    game.preflop_betting(button=button, round_limit=PRE_FLOP_LIMIT)
+    print(pot.total)
+    if dealer.active_players_count(players=players) > 1:
+        print("FLOP")
+        flop = dealer.deal_flop()
+        print(flop)
+        game.flop_betting(button=button, round_limit=FLOP_LIMIT)
+        print(pot.total)
+        if dealer.active_players_count(players=players) > 1:
+            print("TURN")
+            turn = dealer.deal_turn()
+            print(turn)
+            game.turn_betting(button=button, round_limit=TURN_LIMIT)
+            print(pot.total)
+        else:
+            print("Only one player remains. The hand is over.")
+    else:
+        print("Only one player remains. The hand is over.")
+    
 if __name__ == "__main__":
     main()
