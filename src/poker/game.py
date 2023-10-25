62b1cd1452424d7b9b66947d25256e304fbe989e
“quac88” <mac2@thrasher.com>
2023-10-25 02:41:44 -0500

    fix: fix award pot, betting round, and determine winner

--- src/poker/game.py
+++ src/poker/game.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 import random
 from cardecky import Deck, HandRanker
+from enum import Enum
+
 
 class Player:
     def __init__(self, player_ID: int, stack: int, hand: list, status: bool, chips_in_play: int) -> None:
@@ -10,45 +12,39 @@ class Player:
         self.status: bool = status
         self.chips_in_play: int = chips_in_play
 
-    # for printing and debugging
     def __str__(self) -> str:
+        """Representation of a player."""
         return str(self.player_ID)
 
-    # post the ante
+    def can_bet(self, amount: int) -> bool:
+        """Check if a player can bet a certain amount."""
+        return amount <= self.stack
+
+    def bet(self, amount: int, pot: Pot) -> None:
+        """Make a bet or raise."""
+        self.stack -= amount
+        self.chips_in_play += amount
+        pot.add_to_pot(amount)
+
     def post_ante(self, ante: int, pot: Pot) -> None:
-        # check if the player has enough chips to post the ante
-        if ante > self.stack:
-            ante = self.stack
-        self.stack -= ante
-        self.chips_in_play += ante
-        pot.total += ante
+        """Post the ante."""
+        self.bet(ante, pot)
 
-    # place a raise - we can't use the word raise as it is a keyword of python
-    def raise_pot(self, amount: int, pot: Pot) -> None:
-        # check if the player has enough chips to raise
-        if amount > self.stack:
-            amount = self.stack
-        self.stack -= amount
-        self.chips_in_play += amount
-        pot.total += amount
-
-
-    # make a call
     def call(self, amount: int, pot: Pot) -> None:
-        # check if the player has enough chips to call
-        if amount > self.stack:
-            amount = self.stack
-        self.stack -= amount
-        self.chips_in_play += amount
-        pot.total += amount
+        """Make a call."""
+        self.bet(amount, pot)
 
-    # check
     def check(self) -> None:
+        """Check."""
         pass
 
-    # fold
     def fold(self) -> None:
+        """Fold the hand."""
         self.status = False
+    
+    # check if player is bust
+    def is_bust(self) -> bool:
+        return self.stack == 0
 
     def __hash__(self) -> int:
         return hash(self.player_ID)
@@ -63,65 +59,67 @@ class Pot:
     def add_to_pot(self, amount: int) -> None:
         self.total += amount
 
-    def award_pot(self, winners: list[Player]) -> None:
-        # Split pot among winners
-        split_amount = self.total // len(winners) # Using integer division for clarity
-        for winner in winners:
-            winner.stack += split_amount
-        # award any leftover chips to the first player in the list
-        leftover: int = self.total - split_amount * len(winners)
-        winners[0].stack += leftover
-        
+    def award_pot(self, winners: list[Player]):
+        if len(winners) > 1:
+            split_amount = self.total // len(winners)
+            for winner in winners:
+                winner.stack += split_amount
+            self.total = 0
+        else:
+            winner: Player = winners[0] 
+            winner.stack += self.total
+            self.total = 0
+
+
 class Dealer:
     def __init__(self, pot: Pot, deck: Deck, button: int = 0, current_bet: int = 0):
         self.pot: Pot = pot
         self.deck: Deck = deck
         self.button: int = button
         self.current_bet: int = current_bet
+        self.board: list = []  # To store the flop and turn cards
 
-    # Rhode Island Hold'em
     def deal_hand(self, players: list[Player]) -> None:
         for player in players:
-            if player is not None:  # Only deal to non-empty seats
+            if player is not None:
                 player.hand = self.deck.deal_cards(1)
-                # this error can be ignored as we will never not have enough cards
 
-    # move the button (dealer seat) to the next player
     def move_button(self, players: list[Player]) -> None:
         if players[self.button] is None:
             self.button = 0
         else:
             self.button = (self.button + 1) % len(players)
 
-    # we don't need blind setting functions for now
-
-    # deal the flop
     def deal_flop(self) -> list:
-        return self.deck.deal_cards(1)
+        flop = self.deck.deal_cards(1)
+        self.board.extend(flop)
+        return flop
 
-    # deal the turn
     def deal_turn(self) -> list:
-        return self.deck.deal_cards(1)
+        turn = self.deck.deal_cards(1)
+        self.board.extend(turn)
+        return turn
 
-    # count the number of active players
     def active_players_count(self, players: list[Player]):
         return sum(1 for player in players if player.status)
-    
-    # determine the winner
-    def determine_winner(self, players: list[Player]) -> Player:
-        best_rank = float('inf')  # Initialize to infinity
-        winning_player = None
+
+    def determine_winner(self, players: list[Player]) -> list[Player]:
+        best_rank = None
+        winners = []
 
         for player in players:
-            if player.status:  # If the player is still active
-                rank, _ = HandRanker.rank_hand(player.hand)
-                if rank < best_rank:
-                    best_rank = rank
-                    winning_player = player
-                # Handle tie-breakers as needed
+            if player.status is True:
+                combined_hand: list = player.hand + self.board
+                ranked_hand = HandRanker.rank_hand(combined_hand)
 
-        return winning_player
+                if best_rank is None or (ranked_hand and ranked_hand[0] < best_rank):
+                    best_rank = ranked_hand[0]
+                    winners = [(player, ranked_hand)]
+                elif ranked_hand and ranked_hand[0] == best_rank:
+                    winners.append((player, ranked_hand))
 
+
+            return [winner for winner, _ in winners]
 
 class Table:
     def __init__(self, seats) -> None:
@@ -133,58 +131,62 @@ class Table:
     def seat_player(self, player, seat) -> None:
         self.seats[seat] = player
 
+class PlayerAction(Enum):
+    FOLD = 1
+    CHECK = 2
+    CALL = 3
+    RAISE = 4
+
 class Game:
-    def __init__(self, players: list, dealer: Dealer, betting_limit: int, current_bet: int = 0) -> None:
-        self.players: list = players
-        self.dealer: Dealer = dealer
-        self.betting_limit: int = betting_limit
-        self.current_bet: int = current_bet
-        self.num_players: int = len(self.players)
+    def __init__(self, players, dealer, betting_limit, current_bet=0):
+        self.players = players
+        self.dealer = dealer
+        self.betting_limit = betting_limit
+        self.current_bet = current_bet
+        self.num_players = len(self.players)
 
-    def betting_round(self, button, start_offset, round_limit) -> None:
-        current_bet = 0 # Track the current bet
-        raise_occurred = True # Track if a raise occurred
-        last_raiser = None # Track the last raiser
-        players_acted = set()  # Track players who have acted
-
-        start_position = (button + start_offset) % self.num_players # start at three players left of the button preflop and one player left postflop
+    def betting_round(self, button, start_offset, round_limit):
+        current_bet = 0
+        raise_occurred = True
+        last_raiser = None
+        players_acted = set()
+        start_position = (button + start_offset) % self.num_players
 
         while raise_occurred:
             raise_occurred = False
-            for i in range(self.num_players): # Iterate through the players
+            for i in range(self.num_players):
                 current_player = (i + start_position) % self.num_players
+                player = self.players[current_player]
 
-                player = self.players[current_player]  # Assign the player for the current turn
-
-                if player is None or not player.status: # Skip empty seats and folded players
+                if player is None or not player.status:
                     continue
 
-                if player in players_acted:  
+                if player in players_acted:
                     if player != last_raiser or not raise_occurred:
                         continue
 
                 if current_bet == 0:
-                    available_actions = [1, 3]  # check or raise
+                    available_actions = [PlayerAction.CHECK, PlayerAction.RAISE]
                 elif player == last_raiser:
-                    continue  # Skip the last raiser's turn if there's no re-raise after their action
+                    continue
                 else:
-                    available_actions = [0, 2, 3]  # fold, call, or raise
+                    available_actions = [PlayerAction.FOLD, PlayerAction.CALL, PlayerAction.RAISE]
 
                 action = random.choice(available_actions)
                 players_acted.add(player)
 
-                if action == 0:
+                if action == PlayerAction.FOLD:
                     player.fold()
                     print(f"Player {player.player_ID} folded")
-                elif action == 1:
+                elif action == PlayerAction.CHECK:
                     player.check()
                     print(f"Player {player.player_ID} checked")
-                elif action == 2:
+                elif action == PlayerAction.CALL:
                     player.call(amount=current_bet, pot=self.dealer.pot)
                     print(f"Player {player.player_ID} called {current_bet}")
-                elif action == 3:
+                elif action == PlayerAction.RAISE:
                     raise_amount = current_bet + round_limit
-                    player.raise_pot(amount=raise_amount, pot=self.dealer.pot)
+                    player.bet(amount=raise_amount, pot=self.dealer.pot)
                     current_bet = raise_amount
                     self.dealer.current_bet = raise_amount
                     raise_occurred = True
@@ -195,11 +197,11 @@ class Game:
             if not raise_occurred:
                 break
 
-    def preflop_betting(self, button, round_limit) -> None:
+    def preflop_betting(self, button, round_limit):
         self.betting_round(button=button, start_offset=3, round_limit=round_limit)
 
-    def flop_betting(self, button, round_limit) -> None:
+    def flop_betting(self, button, round_limit):
         self.betting_round(button=button, start_offset=1, round_limit=round_limit)
 
-    def turn_betting(self, button, round_limit) -> None:
+    def turn_betting(self, button, round_limit):
         self.betting_round(button=button, start_offset=1, round_limit=round_limit)
