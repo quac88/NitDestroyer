e03c2c40946402537fd7b51821ef74d521271741
“quac88” <mac2@thrasher.com>
2023-10-26 14:47:52 -0500

    refactor: use qqdm instead of tqdm for progress bar. Add comments for better readability

--- src/poker/run.py
+++ src/poker/run.py
@@ -1,4 +1,4 @@
-from tqdm import tqdm
+from qqdm import qqdm, format_str
 import time
 from multiprocessing import Pool
 import timeit
@@ -13,7 +13,7 @@ TURN_LIMIT = 4
 
 START_STACK = 200
 
-def play_round(players, dealer, pot, game, button):
+def play_round(players, dealer, pot, game, button) -> None:
     # reset each player's hand and status
     for player in players:
         player.hand = []
@@ -29,64 +29,50 @@ def play_round(players, dealer, pot, game, button):
 
     # Deal the hands to players
     dealer.deal_hand(players=players)
-    for player in players:
-        print(f"Player{player.player_ID}'s hand: {player.hand}")
 
     # Betting for pre-flop
-    print("PRE-FLOP")
     game.preflop_betting(button=button, round_limit=PRE_FLOP_LIMIT)
-    print(f"Pot total after PRE-FLOP: {pot.total}")
 
+    # If there is only one player left, award the pot and move on to the next round
     if dealer.active_players_count(players=players) <= 1:
-        print("Only one player remains. The hand is over.")
         winners = [player for player in players if player.status]
         pot.award_pot(winners)
         pot.reset_pot()
         return
 
-    # Deal the flop and betting for flop
-    print("\nFLOP")
+    # Deal the flop and begin betting round for the flop
     flop = dealer.deal_flop()
-    print(f"Flop cards: {flop}")
     game.flop_betting(button=button, round_limit=FLOP_LIMIT)
-    print(f"Pot total after FLOP: {pot.total}")
 
+    # If there is only one player left, award the pot and move on to the next round
     if dealer.active_players_count(players=players) <= 1:
-        print("Only one player remains. The hand is over.")
         winners = [player for player in players if player.status]
         pot.award_pot(winners)
         pot.reset_pot()
         return
 
-    # Deal the turn and betting for turn
-    print("\nTURN")
+    # Deal the turn and begin betting round for the turn
     turn = dealer.deal_turn()
-    print(f"Turn card: {turn}")
     game.turn_betting(button=button, round_limit=TURN_LIMIT)
-    print(f"Pot total after TURN: {pot.total}")
     if dealer.active_players_count(players=players) <= 1:
-        print("Only one player remains. The hand is over.")
         pot.award_pot([player for player in players if player.status])
         pot.reset_pot()
+    # if there is more than one player left, determine the winner
     else:
         winners = dealer.determine_winner(players=players)
         if winners:
             pot.award_pot(winners)
-            for winner in winners:
-                print(f"\nPlayer {winner.player_ID} wins with hand: {winner.hand} and is awarded a share of the pot!")
         pot.reset_pot()
 
-def play_round_wrapper(args):
+# Passing a tuple to the wrapper function allows us to use the imap_unordered function from the multiprocessing module
+def play_round_wrapper(args) -> None:
     players, dealer, pot, game, button, round_number = args
-    print(f"\n========== ROUND {round_number} ==========")
     play_round(players=players, dealer=dealer, pot=pot, game=game, button=button)
-    for player in players:
-        print(f"Player{player.player_ID} chip count: {player.stack}")  # Update chip stacks
     button = (button + 1) % len(players)  # Move the button
 
-NUM_ROUNDS = 1_000_000
+NUM_ROUNDS: int = 1_000_000
 def main() -> None:
-    # Initial setup
+    ##### Initial setup #####
     deck = Deck()
     pot = Pot()
     dealer = Dealer(pot=pot, deck=deck)
@@ -99,16 +85,18 @@ def main() -> None:
     dealer.move_button(players=players)
     button = dealer.button
     game = Game(players=players, dealer=dealer, betting_limit=PRE_FLOP_LIMIT)
+    #####  End initial setup #####
 
-    # Create a tqdm progress bar
-    with tqdm(total=NUM_ROUNDS, desc="Rounds", disable=True) as progress_bar:
-        # Create a pool of processes
-        with Pool(processes=20) as pool:  # Adjust the number of processes as needed
-            args = [(players, dealer, pot, game, button, i + 1) for i in range(NUM_ROUNDS)]
-            for _ in pool.imap_unordered(play_round_wrapper, args):
-                progress_bar.update(1)  # Update the progress bar for each completed round
+    # Create a list of arguments to pass to the play_round function
+    args: list[tuple[list[Player], Dealer, Pot, Game, int, int]] = [(players, dealer, pot, game, button, i + 1) for i in range(NUM_ROUNDS)]
+    
+    # Create a pool of processes
+    with Pool(processes=20) as pool:  # Adjust the number of processes as needed for the CPU
+        # Use qqdm to track progress and dispalay a very pretty progress bar
+        for _ in qqdm(pool.imap_unordered(func=play_round_wrapper, iterable=args), total=NUM_ROUNDS, desc="Rounds"):
+            pass  # qqdm will handle the progress automatically
     
 if __name__ == "__main__":
     # check the time it takes to run
-    execution_time = timeit.timeit(main, number=1)
-    print(f"Execution time for {NUM_ROUNDS} rounds: {execution_time:.9f} seconds")    
+    execution_time: float = timeit.timeit(stmt=main, number=1)
+    print(f"Execution time for {NUM_ROUNDS} rounds: {execution_time:.9f} seconds")
