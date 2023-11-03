a7623d428c3502aafa564f3ba8368d0af7ac5b84
“quac88” <mac2@thrasher.com>
2023-11-03 18:33:57 -0500

    feat: remove game class testing for now

--- tests/test_game.py
+++ tests/test_game.py
@@ -188,43 +188,6 @@ class TestTable(unittest.TestCase):
         # Test floating-point seat index
         with self.assertRaises(IndexError, msg="Should raise error for floating-point seat index"):
             self.table.seat_player(player=player, seat=2.5)
-            
-class TestGame(unittest.TestCase):
-    def setUp(self) -> None:
-        """Setup before each test"""
-        self.players: list[Player] = [
-            Player(player_ID=0, stack=200, hand=[], status=True, chips_in_play=0),
-            Player(player_ID=1, stack=200, hand=[], status=True, chips_in_play=0)
-        ]
-        self.dealer = Dealer(pot=Pot(), deck=Deck())
-        self.game = Game(players=self.players, dealer=self.dealer, betting_limit=2)
 
-    @patch("random.choice")
-    def test_preflop_betting(self, mock_random_choice) -> None:
-        """Test preflop betting"""
-        mock_random_choice.side_effect = [PlayerAction.CHECK, PlayerAction.CHECK]
-        self.game.preflop_betting(button=0, round_limit=2)
-
-        for player in self.players:
-            self.assertEqual(player.chips_in_play, 0)
-
-    @patch("random.choice")
-    def test_flop_betting(self, mock_random_choice) -> None:
-        """Test flop betting"""
-        mock_random_choice.side_effect = [PlayerAction.CALL, PlayerAction.RAISE, PlayerAction.CALL]
-        self.game.flop_betting(button=0, round_limit=10)
-
-        self.assertEqual(self.dealer.pot.total, 30)
-        self.assertEqual(self.players[0].chips_in_play, 20)
-        self.assertEqual(self.players[1].chips_in_play, 10)
-
-    @patch("random.choice")
-    def test_turn_betting(self, mock_random_choice) -> None:
-        """Test turn betting"""
-        mock_random_choice.side_effect = [PlayerAction.CALL, PlayerAction.RAISE, PlayerAction.FOLD]
-        self.game.turn_betting(button=0, round_limit=4)
-
-        self.assertEqual(self.dealer.pot.total, 20)
-        self.assertEqual(self.players[0].chips_in_play, 10)
-        self.assertFalse(self.players[1].status)
-
+if __name__ == '__main__':
+    unittest.main()
